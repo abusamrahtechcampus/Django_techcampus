9b5574f0ca03a81f69da04251e0ea53a94b2e9dc
abusamrahtechcampus <abusamrahtechcampus@gmail.com>
2019-05-20 05:15:14 +0300

    add all flse

---
+++ firstproject/boards/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Board(models.Model):
+	name = models.CharField(max_length=100,unique=True)
+	description = models.CharField(max_length=400)
+
+	def __str__(self):
+		return self.name
+
+
+
+
+
+
+
+class Topic(models.Model):
+	supject = models.CharField(max_length=200)
+	Created_by = models.ForeignKey(User,related_name='topics',on_delete=models.CASCADE)
+	board = models.ForeignKey(Board, related_name = 'topics',on_delete=models.CASCADE)
+	Created_dt = models.DateTimeField(auto_now_add = True)
+
+class Post(models.Model):
+	message = models.TextField(max_length=4000)
+	topic = models.ForeignKey(Topic, related_name='posts',on_delete=models.CASCADE)
+	Created_by = models.ForeignKey(User,related_name='posts',on_delete=models.CASCADE)
+	Created_dt = models.DateTimeField(auto_now_add=True)
---
+++ firstproject/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\django_project_mysql\firstproject\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
